28a11f55c631945e7fda8dd254f4341b13ac833c
mohu21 <104739700+mohu21@users.noreply.github.com>
2022-05-02 18:59:26 +0200

    Update arch-base.sh

--- arch-base.sh
+++ arch-base.sh
@@ -9,20 +9,5 @@ echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.local arch" >> /etc/hosts
-echo root:pw | chpasswd
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S grub efibootmgr networkmanager base-devel bash-completion openssh rsync os-prober alacritty firefox nvidia
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-systemctl enable sshd
-
-useradd -m moritz
-echo moritz:pw | chpasswd
-
-echo "moritz ALL=(ALL) ALL" >> /etc/sudoers.d/moritz
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
